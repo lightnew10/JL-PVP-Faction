5e47c23545a31727c6175f9da5cb1529cfc493ae
BuildTools <unconfigured@null.spigotmc.org>
2022-12-12 18:40:47 +0100

    update API

--- src/main/java/fr/lightnew/api/FactionAPI.java
+++ src/main/java/fr/lightnew/api/FactionAPI.java
@@ -2,11 +2,8 @@ package fr.lightnew.api;
 
 import fr.lightnew.MainFac;
 import fr.lightnew.faction.Faction;
-import org.bukkit.ChatColor;
-import org.bukkit.Chunk;
-import org.bukkit.NamespacedKey;
+import fr.lightnew.listeners.PlayerManager;
 import org.bukkit.entity.Player;
-import org.bukkit.persistence.PersistentDataType;
 
 public class FactionAPI {
 
@@ -15,7 +12,6 @@ public class FactionAPI {
     }
 
     public String getNameFactionChunk(Player player) {
-        Chunk chunk = player.getWorld().getChunkAt(player.getLocation());
-        return chunk.getPersistentDataContainer().has(new NamespacedKey(MainFac.instance, "faction"), PersistentDataType.INTEGER) ? ChatColor.RED + String.valueOf(chunk.getPersistentDataContainer().get(new NamespacedKey(MainFac.instance, "faction_name"), PersistentDataType.STRING)) : ChatColor.GREEN + "Wilderness";
+        return PlayerManager.getClaimHere(player);
     }
 }
